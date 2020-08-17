eebcb4ecd86b9490b804e57fac0e196f7ebe3a40
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2020-08-16 21:36:49 -0500

    Feat: Relations between Movies and Turns
    asd

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     //
+    /**
+     * A Movie maybe has a many turns
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function turns()
+    {
+        return $this->belongsToMany(Turn::class);
+    }
 }
--- app/Models/Turn.php
+++ app/Models/Turn.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Turn extends Model
 {
     //
+    /**
+     * A Turn maybe have many movies
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function movies()
+    {
+        return $this->belongsToMany(Movie::class);
+    }
 }
