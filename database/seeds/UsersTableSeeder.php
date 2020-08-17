57d927243168eb1c4fce7048f19a20b1c810dca5
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2020-08-17 02:21:58 -0500

    Feat: Populate users table with seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function run()
+    {
+        factory(User::class, random_int(20, 40))->create();
+    }
+}
